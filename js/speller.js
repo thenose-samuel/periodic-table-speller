5a7f460a1d2c506445019cd1c6a80acb9edd2b29
Samuel Khongthaw <skthaw1@gmail.com>
2023-04-22 08:36:58 +0530

    added speller check function

--- js/speller.js
+++ js/speller.js
@@ -1,13 +1,6 @@
-export default {
-  check,
-  lookup,
-};
-
 var elements;
 
 await loadPeriodicTable();
-
-// ****************************
 
 async function loadPeriodicTable() {
   elements = await (await fetch("periodic-table.json")).json();
@@ -15,16 +8,32 @@ async function loadPeriodicTable() {
 }
 
 function check(inputWord) {
-  // TODO: determine if `inputWord` can be spelled
-  // with periodic table symbols; return array with
-  // them if so (empty array otherwise)
+  if (inputWord.length > 0) {
+    for (let element of elements) {
+      let symbol = element.symbol.toLowerCase();
+      if (symbol.length <= inputWord.length) {
+        //did the symbol math the first one or two characters in 'inputWord'
+        if (inputWord.slice(0, symbol.length) == symbol) {
+          //still have characters left?
+          if (inputWord.length > symbol.length) {
+            let res = check(inputWord.slice(symbol.length));
 
-  return ["y", "u", "c", "k", "y"];
+            //matched successfully
+            if (res.length > 0) {
+              return [symbol, ...res];
+            }
+          } else {
+            return [symbol];
+          }
+        }
+      }
+    }
+  }
+
+  return [];
 }
 
 function lookup(elementSymbol) {
-  // TODO: return the element entry based on specified
-  // symbol (case-insensitive)
   for (let element of elements) {
     if (element.symbol.toLowerCase() == elementSymbol) {
       return element;
@@ -32,3 +41,8 @@ function lookup(elementSymbol) {
   }
   return {};
 }
+
+export default {
+  check,
+  lookup,
+};
